b3bcdfe3957b350899941f6cb325496576f7308e
doniahusien <doniahusien@gmail.com>
2024-04-19 18:35:39 +0200

    class & object

---
+++ implementation/v1_to_v17/video4.php
@@ -0,0 +1,9 @@
+<?php
+class AppleDevice {
+
+}
+$iphone8 =  new AppleDevice();
+
+echo "<pre>";
+var_dump($iphone8);
+echo "</pre>";
---
+++ implementation/v1_to_v17/video5.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class AppleDevice {
+    public $ram;
+    public $inch;
+    public $space;
+    public $price;
+    public $color="black";
+    
+}
+$iphone8 =  new AppleDevice();
+$iphone6 =  new AppleDevice();
+echo "<pre>";
+var_dump($iphone8);
+
+var_dump($iphone6);
+echo "</pre>";
